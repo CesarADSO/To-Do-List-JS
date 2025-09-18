04cbcf1302a893f86f125e3911f227cc2896b62e
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-18 14:44:39 -0500

    Agregué la funcionalidad de tareas terminadas o por hacer además del eliminar tareas

--- js/main.js
+++ js/main.js
@@ -7,13 +7,18 @@ const completedList = document.getElementById('cont-to-do-list-complete');
 
 // CREAMOS LA FUNCIÓN QUE NOS PERMITE CREAR UNA NUEVA TAREA A PARTIR DEL FORMULARIO
 // TODA ETIQUETA QUE VAMOS A CREAR ES A PARTIR DE LA MAQUETA HTML PRE EXISTENTE
+
+// ESTA FUNCIÓN SOLO CREA LA ESTRUCTURA DEL HTML EN UN LIMBO O UNA MEMORIA TEMPORAL; AÚN NO LA INSERTA EN LA PÁGINA
 function createToDoItem (textItem) {
+
     // CREAMOS EL NODO O ELEMENTO PADRE O CONTENEDOR PADRE
     const item = document.createElement('div');
     item.classList.add('item-to-do');
+
     // CREAMOS EL NODO HIJO DEL INPUT Y LE AGREGAMOS EL TYPE CHECKBOX
     const checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
+
     // CREAMOS EL SIGUIENTE NODO HIJO PARRAFO
     // A ESTE PÁRRAFO LE ASIGNO EL VALOR DEL ALRGUMENTO DE LA FUNCIÓN ES DECIR LO QUE ESCRIBE EL USUARIO EN EL CAMPO
     const p = document.createElement('p');
@@ -27,13 +32,16 @@ function createToDoItem (textItem) {
     item.appendChild(checkBox);
     item.appendChild(p);
     item.appendChild(deleteBtn);
-    // UTILIZAMOS EL RETURN PARA RETORNAR O DAR RESPUESTA AL ELEMENTO CREADO YA QUE LO UTILIZAREMOS EN UNA FUCIÓN MÁS ADELANTE
+
+    // UTILIZAMOS EL RETURN PARA RETORNAR O DAR RESPUESTA AL ELEMENTO CREADO YA QUE LO UTILIZAREMOS EN UNA FUNCIÓN MÁS ADELANTE
     return item;
 
 }
 
 // DETECTAMOS EL EVENTO CLICK SOBRE EL BOTÓN + CON UN EVENTO DE ESCUCHA LISTENER 
+
 // PARA QUE A PARTIR DE ESTE EVENTO SE AGREGUE LA TAREA DENTRO DEL CONTENEDOR #cont-to-do-list 
+
 addBtn.addEventListener('click', () => {
     const textItem = input.value.trim();
     if (textItem == "") {
@@ -42,6 +50,35 @@ addBtn.addEventListener('click', () => {
     else {
         const newItem = createToDoItem (textItem);
         toDoList.appendChild(newItem);
+
+
+        eventsToItem(newItem)
+
+
         input.value = "";
     }
 });
+
+// LA SIGUIENTE FUNCIÓN NOS PERMITIRÁ AGREGAR EL FUNCIONAMIENTO PRINCIPAL SOBRE LAS TAREAS ES DECIR MARCAR LA TAREA COMO REALIZADA O COMPLETADA O EN DADO CASO ELIMINARLA
+
+function eventsToItem (item) {
+
+    // UTILIZAMOS querySelector PARA CAPTURAR EL INPUT Y EL BUTTON QUE ESTÁN DENTRO DEL ITEM
+    const checkBox = item.querySelector('input');
+    const deleteBtn = item.querySelector('button');
+
+    // COMPLETAR LA TAREA
+    checkBox.addEventListener('change', () => {
+        if (checkBox.checked) {
+            completedList.appendChild(item);
+        }
+        else {
+            toDoList.appendChild(item);
+        }
+    });
+
+    // ELIMINAR LA TAREA
+    deleteBtn.addEventListener('click', () => {
+        item.remove();
+    });
+}
